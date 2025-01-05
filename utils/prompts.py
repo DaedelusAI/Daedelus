ce8874fdb1e933a33c037b83015c5f59f03fca91
DaedelusAI <ainew9730@gmail.com>
2025-01-04 19:59:32 -0800

    Create prompts.py

---
+++ utils/prompts.py
@@ -0,0 +1,10 @@
+TOKEN_LAUNCH_GUIDE = """
+Welcome to the Token Launch Guide!
+
+1. Use the /launch command to create a new token.
+   Syntax: /launch <name> <symbol> <decimals> <supply>
+
+2. Check the status of your launch with /status.
+
+3. Need help? Ask me anything about tokenomics or Solana token standards.
+"""
